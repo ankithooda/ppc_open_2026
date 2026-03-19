716b8c2e79f78aa9c62372d5dc2177db994ba216
Ankit <ankithooda23@gmail.com>
2026-03-19 17:15:07 +0530

    Added harness for running tests and benchmarks

---
+++ chapter_2_case_study/driver.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void step(float* r, const float* d, int n);
+
+int main () {
+    constexpr int n = 3;
+    const float d[n*n] = {
+        0, 8, 2,
+        1, 0, 9,
+        4, 5, 0,
+    };
+    float r[n*n];
+    step(r, d, n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << r[i*n + j] << " ";
+        }
+        std::cout << "\n";
+    }
+  return 0;
+}
