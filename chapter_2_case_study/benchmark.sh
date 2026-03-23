810e9990a24dc44cf6eaabc71225df993017afc4
Ankit <ankithooda23@gmail.com>
2026-03-23 16:55:27 +0530

    Use optimized build to benchmark

--- chapter_2_case_study/benchmark.sh
+++ chapter_2_case_study/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm ./bin/benchmark_test
-g++ -o bin/benchmark_test benchmark.cc $1/step.cc
+g++ -o bin/benchmark_test -g -O3 -march=native -std=c++17 benchmark.cc $1/step.cc
 ./bin/benchmark_test
 
