3311b562a70dd8ff6515389338d8463b8097b6a0
Ankit <ankithooda23@gmail.com>
2026-03-23 17:14:37 +0530

    Add openmp optimization

---
+++ chapter_2_case_study/openmp/step.cc
@@ -0,0 +1,27 @@
+#include <limits>
+
+void step(float* r, const float* d, int n);
+
+void step(float* r, const float* d, int n) {
+
+    #pragma omp parallel for
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            float min_ij = std::numeric_limits<double>::infinity();
+
+            for (int k = 0; k < n; k++) {
+                float value = d[i + k * n] + d[k + j * n];
+
+                if (value < min_ij) {
+                    min_ij = value;
+                }
+            }
+
+            // Set in result data for i, j
+            r[i + j * n] = min_ij;
+        }
+    }
+}
