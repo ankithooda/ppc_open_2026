037c685a72ee42c4eeb755ca81811f4c2fea6445
Ankit <ankithooda23@gmail.com>
2026-03-24 16:40:48 +0530

    Gathered perf data for v0 and v1 showing LLC miss improvements

--- chapter_2_case_study/benchmark.sh
+++ chapter_2_case_study/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm ./bin/benchmark_test
-g++ -o bin/benchmark_test -fopenmp -g -O3 -march=native -std=c++17 benchmark.cc $1/step.cc
-./bin/benchmark_test $2
+rm ./bin/$1_benchmark_test
+g++ -o bin/$1_benchmark_test -fopenmp -g -O3 -march=native -std=c++17 benchmark.cc $1/step.cc
+./bin/$1_benchmark_test $2
 
