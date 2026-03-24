738cf0d45d1f5c4b0e5a6c28905b5d0fbdd2b934
Ankit <ankithooda23@gmail.com>
2026-03-24 15:13:07 +0530

    Update makefile

--- chapter_2_case_study/generate_plot_data.sh
+++ chapter_2_case_study/generate_plot_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dest_1="$1/elpased_time_data_1"
-dest_2="$1/useful_ops_data_1"
+dest_1="$1/elpased_time_data"
+dest_2="$1/useful_ops_data"
 rm $dest_1 $dest_2
 echo -e "#\tX\tY" > $dest_1
 echo -e "#\tX\tY" > $dest_2
@@ -9,4 +9,8 @@ echo -e "#\tX\tY" > $dest_2
 for ((size = 200; size <= 400; size = size + 200)); do
     make benchmark T=$1 N=$size | grep AWK_OUTPUT | awk -v dest_1=$dest_1 -v dest_2=$dest_2 '{print $2"\t"$3 >> (dest_1); print $2"\t"$4 >> (dest_2)}'
 done
+
+echo "Generated file $dest_1"
+echo "Generated file $dest_2"
+
 
