46eec2b1a87eee5163a90794ae759c80503b63b5
Ankit <ankithooda23@gmail.com>
2026-03-25 23:53:28 +0530

    Print expected out in test.sh. Remove RUN_LOG, we now have better benchmarking system in place.

--- chapter_2_case_study/test.sh
+++ chapter_2_case_study/test.sh
@@ -2,7 +2,12 @@
 
 g++ -o bin/$1_test driver.cc $1/step.cc
 ./bin/$1_test > bin/test_output
+
+echo "Your Output"
 cat bin/test_output
+
+echo "Expected Output"
+cat expected_output
 
 echo "Showing difference with expected output"
 diff -q bin/test_output expected_output
